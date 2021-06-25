85b534cd6fde9e8698f6cdc53efe26d2d2ae2a3c
Cuong <cuong.vvk.95@gmail.com>
2021-06-25 10:25:12 +0700

    da sua loi

--- DoAnWeb/Controllers/TrangChuController.cs
+++ DoAnWeb/Controllers/TrangChuController.cs
@@ -19,6 +19,39 @@ namespace DoAnWeb.Controllers
         // GET: TrangChu
         public ActionResult Index()
         {
+            var hoamoi = Layhoamoi(6);
+            return View(hoamoi);
+        }
+        public ActionResult Loai()
+        {
+            var loai = from l in data.LoaiSPs select l;
+            return PartialView(loai);
+
+        }
+        public ActionResult MauSac()
+        {
+            var mausac = from ms in data.MauSacs select ms;
+            return PartialView(mausac);
+        }
+        public ActionResult ChuDe()
+        {
+            var chude = from cd in data.ChuDes select cd;
+            return PartialView(chude);
+        }
+        public ActionResult HoatheoCD(int id)
+        {
+            var hoa = from h in data.SanPhans where h.MaCD == id select h;
+            return View(hoa);
+        }
+        public ActionResult HoatheoLoai(int id)
+        {
+            var hoa = from h in data.SanPhans where h.MaLoai == id select h;
+            return View(hoa);
+        }
+        public ActionResult HoatheoMS(int id)
+        {
+            var hoa = from h in data.SanPhans where h.MaMau == id select h;
+            return View(hoa);
         }
     }
 }
